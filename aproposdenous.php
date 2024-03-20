baa1a9848a8e80b12c94bcd92b2604076ab1727c
Rizou Mlk <99905798+Rizouu@users.noreply.github.com>
2024-03-20 09:54:10 +0100

    Add files via upload

---
+++ aproposdenous.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qui sommes-nous? - CarChoix</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Italiana&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&display=swap">
+    <link rel="stylesheet" href="aproposdenous.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="logo.png" alt="Logo CarChoix">
+            <h2> Qui sommes-nous? </h2>
+
+        </div>
+        <nav>
+            <ul>
+            <li><a href="index.php">Accueil</a></li>
+                <li><a href="login.php">Connexion</a></li>
+                <li><a href="register.php">Inscription</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>
+        Bienvenue chez CarChoix ! 
+        </h1>
+        <p class="titre2">
+        Nous sommes ravis de vous accueillir sur CarChoix, votre destination en ligne lors de l'achat de votre prochaine voiture. 
+        </p>
+        <div>
+            <img class="image1" src="./img1.jpg">
+            <p class="texte1">
+            Il n'y a rien de plus gratifiant que cette confiance dans la reussite. C'est pourquoi, chez Carchoix, nous mettons à la disposition des acheteurs de voitures toutes les ressources nécessaires pour une expérience sur mesure, de A à Z. Désormais, ils peuvent effectuer leurs achats à leur convenance, selon leurs désirs, afin de trouver la voiture idéale qui correspond parfaitement à leur style de vie et à leur budget, parmi l'un des plus vastes inventaires de véhicules de tout le pays.
+            </p>
+        </div>
+        <div>
+            <p class="texte2">
+            Que vous recherchiez une berline économe en carburant, un SUV spacieux ou une voiture de sport élégante, CarChoix est là pour vous aider à trouver la voiture qui vous convient le mieux. Explorez notre site dès maintenant et commencez votre voyage vers l'achat de la voiture de vos réves.
+            </p>
+            <img class="image2" src="./img2.jpg">
+        </div>
+        <div>
+            <p class="texte3">
+            Rejoignez la communauté CarChoix et prenez la route en toute confiance !
+            </p>
+        </div>
+    </main>
+    <footer>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Se connecter</a></li>
+            <li><a href="#">S'inscrire</a></li>
+            <li><a href="#">Contactez-nous</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Vos recherches</a></li>
+            <li><a href="#">Fichier d'informations</a></li>
+            <li><a href="#">Comparez</a></li>
+            <li><a href="#">Nos engagements</a></li>
+        </ul>
+    </footer>
+</body>
+</html>

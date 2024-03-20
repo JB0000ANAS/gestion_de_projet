0897447e8f802249241289e945c8799febdcf051
Rizou Mlk <99905798+Rizouu@users.noreply.github.com>
2024-03-20 09:52:35 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contactez-nous - CarChoix</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_contact.css">
 </head>
 <body>
     <header>

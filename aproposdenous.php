34c14c691522696896a31c60022514161ec97d26
Jebali ANAS <100607796+JB0000ANAS@users.noreply.github.com>
2024-03-21 03:17:59 +0100

    Update aproposdenous.php

--- aproposdenous.php
+++ aproposdenous.php
@@ -16,15 +16,6 @@
         <div class="logo">
             <img src="logo.png" alt="Logo CarChoix">
             <h2> Qui sommes-nous? </h2>
-
-        </div>
-        <nav>
-            <ul>
-            <li><a href="index.php">Accueil</a></li>
-                <li><a href="login.php">Connexion</a></li>
-                <li><a href="register.php">Inscription</a></li>
-            </ul>
-        </nav>
     </header>
     <main>
         <h1>
@@ -36,33 +27,31 @@
         <div>
             <img class="image1" src="./img1.jpg">
             <p class="texte1">
-            Il n'y a rien de plus gratifiant que cette confiance dans la reussite. C'est pourquoi, chez Carchoix, nous mettons à la disposition des acheteurs de voitures toutes les ressources nécessaires pour une expérience sur mesure, de A à Z. Désormais, ils peuvent effectuer leurs achats à leur convenance, selon leurs désirs, afin de trouver la voiture idéale qui correspond parfaitement à leur style de vie et à leur budget, parmi l'un des plus vastes inventaires de véhicules de tout le pays.
+                Il n'y a rien de plus gratifiant que cette confiance dans la rÃ©ussite. C'est pourquoi, chez Carchoix, nous mettons Ã  la disposition des acheteurs de voitures toutes les ressources nÃ©cessaires pour une expÃ©rience sur mesure, de A Ã  Z. DÃ©sormais, ils peuvent effectuer leurs achats Ã  leur convenance, selon leurs dÃ©sirs, afin de trouver la voiture idÃ©ale qui correspond parfaitement Ã  leur style de vie et Ã  leur budget, parmi l'un des plus vastes inventaires de vÃ©hicules de tout le pays.
             </p>
         </div>
         <div>
-            <p class="texte2">
-            Que vous recherchiez une berline économe en carburant, un SUV spacieux ou une voiture de sport élégante, CarChoix est là pour vous aider à trouver la voiture qui vous convient le mieux. Explorez notre site dès maintenant et commencez votre voyage vers l'achat de la voiture de vos réves.
+        <p class="texte2">
+                Que vous recherchiez une berline Ã©conome en carburant, un SUV spacieux ou une voiture de sport Ã©lÃ©gante, CarChoix est lÃ  pour vous aider Ã  trouver la voiture qui vous convient le mieux. Explorez notre site dÃ¨s maintenant et commencez votre voyage vers l'achat de la voiture de vos rÃªves.
             </p>
             <img class="image2" src="./img2.jpg">
         </div>
         <div>
             <p class="texte3">
-            Rejoignez la communauté CarChoix et prenez la route en toute confiance !
+            Rejoignez la communautÃ© CarChoix et prenez la route en toute confiance !
             </p>
         </div>
     </main>
     <footer>
         <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Se connecter</a></li>
-            <li><a href="#">S'inscrire</a></li>
-            <li><a href="#">Contactez-nous</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="login.php">Connexion</a></li>
+            <li><a href="register.php">Inscription</a></li>
         </ul>
         <ul>
-            <li><a href="#">Vos recherches</a></li>
-            <li><a href="#">Fichier d'informations</a></li>
-            <li><a href="#">Comparez</a></li>
-            <li><a href="#">Nos engagements</a></li>
+            <li><a href="profile.php">Modifier mon profil</a></li>
+            <li><a href="contact.php">Nous contacter</a></li>
+            <li> <a href="Deconnexion.php">DÃ©connexion</a></li>
         </ul>
     </footer>
 </body>
